86fbf103f7fe71440b260433fdb5df65aee5c063
de-rekk <painganshawnderek@gmail.com>
2026-08-14 17:08:28 +0800

    Added generateQRCodeImage helper method using ZXing

--- src/main/java/ph/edu/dlsu/anilink/service/QRService.java
+++ src/main/java/ph/edu/dlsu/anilink/service/QRService.java
@@ -20,4 +20,20 @@ public class QRService {
         return qrPayload != null && qrPayload.startsWith("ANILINK-RES-");
     }
 
+    private BufferedImage generateQRCodeImage(String text, int width, int height) {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+                }
+            }
+            return image;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }

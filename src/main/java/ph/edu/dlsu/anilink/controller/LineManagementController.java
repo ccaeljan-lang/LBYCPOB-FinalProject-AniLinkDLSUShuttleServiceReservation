26155ee0135165fc995669bf98cadd2c4e4cfee7
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 21:01:00 +0800

    Setup LineManagementController UI bindings and utilities

--- src/main/java/ph/edu/dlsu/anilink/controller/LineManagementController.java
+++ src/main/java/ph/edu/dlsu/anilink/controller/LineManagementController.java
@@ -1,4 +1,51 @@
 package ph.edu.dlsu.anilink.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class LineManagementController {
+    @FXML
+    private TextField lineNameField;
+
+    @FXML
+    private TextField locationAField;
+
+    @FXML
+    private TextField locationBField;
+
+    @FXML
+    private ListView<String> lineListView;
+
+    private final ObservableList<String> lines =
+            FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        lineListView.setItems(lines);
+    }
+
+    private void clearFields() {
+
+        lineNameField.clear();
+        locationAField.clear();
+        locationBField.clear();
+    }
+
+    private void showAlert(
+            Alert.AlertType type,
+            String title,
+            String message) {
+
+        Alert alert = new Alert(type);
+
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
 }

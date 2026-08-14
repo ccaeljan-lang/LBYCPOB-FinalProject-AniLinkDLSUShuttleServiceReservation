e7124e32d294c33e95ccdcc5cd3c09cd5df5f835
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 16:21:59 +0800

    Created initial CategoryLimitRule structure.

--- src/main/java/ph/edu/dlsu/anilink/rules/CategoryLimitRule.java
+++ src/main/java/ph/edu/dlsu/anilink/rules/CategoryLimitRule.java
@@ -1,4 +1,19 @@
 package ph.edu.dlsu.anilink.rules;
 
-public class CategoryLimitRule {
+import ph.edu.dlsu.anilink.interfaces.BookingRule;
+import ph.edu.dlsu.anilink.model.Trip;
+import ph.edu.dlsu.anilink.model.User;
+
+public class CategoryLimitRule implements BookingRule {
+    private static final int MAX_CATEGORY_BOOKINGS = 2;
+
+    @Override
+    public boolean validate(User user, Trip trip) {
+        return true; // Placeholder for future logic
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return "You have reached the maximum number of allowed bookings.";
+    }
 }

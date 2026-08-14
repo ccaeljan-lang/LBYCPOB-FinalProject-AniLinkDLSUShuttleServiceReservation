dc8ddea90b820fdffad708dd690b8741e0016aba
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 15:59:58 +0800

    Created initial BookingRule interface for validation

--- src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
+++ src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
@@ -1,4 +1,8 @@
 package ph.edu.dlsu.anilink.interfaces;
 
+import ph.edu.dlsu.anilink.model.Trip;
+import ph.edu.dlsu.anilink.model.User;
+
 public class BookingRule {
+    boolean validate(User user, Trip trip);
 }

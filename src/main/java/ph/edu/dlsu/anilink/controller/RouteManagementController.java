9ed09cc6622efa051c5582c58238417acae071ff
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 21:24:57 +0800

    Added utility methods for UI alerts and field clearing

--- src/main/java/ph/edu/dlsu/anilink/controller/RouteManagementController.java
+++ src/main/java/ph/edu/dlsu/anilink/controller/RouteManagementController.java
@@ -3,6 +3,7 @@ package ph.edu.dlsu.anilink.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -35,5 +36,28 @@ public class RouteManagementController {
     public void initialize() {
 
         routeListView.setItems(routes);
+    }
+
+    private void clearFields() {
+
+        routeIdField.clear();
+        categoryField.clear();
+        lineField.clear();
+        originField.clear();
+        destinationField.clear();
+    }
+
+    private void showAlert(
+            Alert.AlertType type,
+            String title,
+            String message) {
+
+        Alert alert = new Alert(type);
+
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.showAndWait();
     }
 }

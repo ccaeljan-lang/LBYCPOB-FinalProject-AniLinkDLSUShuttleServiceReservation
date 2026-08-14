f4f5fc7ef5a94801ac759e14faac49163fad0a00
de-rekk <painganshawnderek@gmail.com>
2026-08-14 16:43:31 +0800

    Added DepartureSchedule with scheduleId, route, departureTime, capacity

--- src/main/java/ph/edu/dlsu/anilink/model/DepartureSchedule.java
+++ src/main/java/ph/edu/dlsu/anilink/model/DepartureSchedule.java
@@ -1,4 +1,33 @@
 package ph.edu.dlsu.anilink.model;
 
+import java.time.LocalTime;
+
 public class DepartureSchedule {
+    private Long scheduleId;
+    private Route route;
+    private LocalTime departureTime;
+    private int capacity;
+
+    public DepartureSchedule(Long scheduleId, Route route, LocalTime departureTime, int capacity) {
+        this.scheduleId = scheduleId;
+        this.route = route;
+        this.departureTime = departureTime;
+        this.capacity = capacity;
+    }
+
+    public Long getScheduleId() {
+        return scheduleId;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }

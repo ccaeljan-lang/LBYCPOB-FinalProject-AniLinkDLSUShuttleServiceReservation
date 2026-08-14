9518f22ecf790ab3b64a8b4ef544d3f1e409196d
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 14:55:04 +0800

    Created initial Reservation model in Reservation.java

--- src/main/java/ph/edu/dlsu/anilink/model/Reservation.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Reservation.java
@@ -1,4 +1,34 @@
 package ph.edu.dlsu.anilink.model;
+import java.time.LocalDateTime;
 
 public class Reservation {
+    private Long reservationId;
+    private Passenger passenger;
+    private Trip trip;
+    private LocalDateTime createdAt;
+
+    public Reservation(Long reservationId,
+                       Passenger passenger,
+                       Trip trip) {
+
+        this.reservationId = reservationId;
+        this.passenger = passenger;
+        this.trip = trip;
+        this.createdAt = LocalDateTime.now();
+    }
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }

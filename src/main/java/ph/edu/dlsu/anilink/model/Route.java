a1e87fdb85930605f6cd805c417e8737f00e6a14
de-rekk <painganshawnderek@gmail.com>
2026-08-14 14:17:13 +0800

    Added Route with routeId, origin, destination

--- src/main/java/ph/edu/dlsu/anilink/model/Route.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Route.java
@@ -1,4 +1,29 @@
 package ph.edu.dlsu.anilink.model;
 
 public class Route {
+    private Long routeId;
+    private String origin;
+    private String destination;
+
+    public Route(Long routeId, String origin, String destination) {
+        this.routeId = routeId;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public Long getRouteId() {
+        return routeId;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getRouteName() {
+        return origin + " -> " + destination;
+    }
 }

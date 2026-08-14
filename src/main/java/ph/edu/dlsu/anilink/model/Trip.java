2b4e615dfb1e4c432d201db96f9d43f9c68862ea
de-rekk <painganshawnderek@gmail.com>
2026-08-14 16:54:33 +0800

    Added Trip class with constructor, fields, and updateLocation

--- src/main/java/ph/edu/dlsu/anilink/model/Trip.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Trip.java
@@ -1,4 +1,31 @@
 package ph.edu.dlsu.anilink.model;
 
-public class Trip {
+import ph.edu.dlsu.anilink.interfaces.Trackable;
+
+public class Trip implements Trackable {
+    private Long tripId;
+    private Route route;
+    private DepartureSchedule schedule;
+    private Driver driver;
+    private int capacity;
+    private int seatsTaken;
+    private String status;
+    private double currentLat;
+    private double currentLng;
+
+    public Trip(Long tripId, Route route, DepartureSchedule schedule, int capacity) {
+        this.tripId = tripId;
+        this.route = route;
+        this.schedule = schedule;
+        this.capacity = capacity;
+        this.seatsTaken = 0;
+        this.status = "SCHEDULED";
+    }
+
+    @Override
+    public void updateLocation(double lat, double lng) {
+        this.currentLat = lat;
+        this.currentLng = lng;
+    }
+
 }

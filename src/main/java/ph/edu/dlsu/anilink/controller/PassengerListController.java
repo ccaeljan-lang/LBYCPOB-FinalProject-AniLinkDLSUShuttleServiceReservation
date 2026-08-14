d2c3e3486165553af25566bf148b13610499de37
de-rekk <painganshawnderek@gmail.com>
2026-08-14 20:34:43 +0800

    Implemented PassengerListController with mock data

--- src/main/java/ph/edu/dlsu/anilink/controller/PassengerListController.java
+++ src/main/java/ph/edu/dlsu/anilink/controller/PassengerListController.java
@@ -1,4 +1,32 @@
 package ph.edu.dlsu.anilink.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class PassengerListController {
+
+    @FXML
+    private ListView<String> passengerListView;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private void initialize() {
+        loadPassengers();
+    }
+
+    private void loadPassengers() {
+        passengerListView.getItems().clear();
+        passengerListView.getItems().addAll(
+                "Passenger 1 - Verified",
+                "Passenger 2 - Pending Verification"
+        );
+    }
+
+    @FXML
+    private void handleBack() {
+        System.out.println("Returning to trip details...");
+    }
 }

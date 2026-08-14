9ebec55a502af1f4c9d9675f941edb1d8558bbbd
de-rekk <painganshawnderek@gmail.com>
2026-08-14 20:30:15 +0800

    Implemented AdminDashboardController with stat labels

--- src/main/java/ph/edu/dlsu/anilink/controller/AdminDashboardController.java
+++ src/main/java/ph/edu/dlsu/anilink/controller/AdminDashboardController.java
@@ -1,4 +1,31 @@
 package ph.edu.dlsu.anilink.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class AdminDashboardController {
+
+    @FXML
+    private Label totalUsersLabel;
+
+    @FXML
+    private Label todaysTripsLabel;
+
+    @FXML
+    private Label activeTripsLabel;
+
+    @FXML
+    private Label reservationsLabel;
+
+    @FXML
+    private Label availableSeatsLabel;
+
+    @FXML
+    private void initialize() {
+        totalUsersLabel.setText("Total Users: --");
+        todaysTripsLabel.setText("Today's Trips: --");
+        activeTripsLabel.setText("Active Trips: --");
+        reservationsLabel.setText("Reservations: --");
+        availableSeatsLabel.setText("Available Seats: --");
+    }
 }

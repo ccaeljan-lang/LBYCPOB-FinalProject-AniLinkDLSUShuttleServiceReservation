f3ca1e3c3721191fd02d7debd182d2da140693ee
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-14 21:20:12 +0800

    Setup RouteManagementController UI bindings and list initialization

--- src/main/java/ph/edu/dlsu/anilink/controller/RouteManagementController.java
+++ src/main/java/ph/edu/dlsu/anilink/controller/RouteManagementController.java
@@ -1,4 +1,39 @@
 package ph.edu.dlsu.anilink.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import ph.edu.dlsu.anilink.model.Route;
+
 public class RouteManagementController {
+
+    @FXML
+    private TextField routeIdField;
+
+    @FXML
+    private TextField categoryField;
+
+    @FXML
+    private TextField lineField;
+
+    @FXML
+    private TextField originField;
+
+    @FXML
+    private TextField destinationField;
+
+    @FXML
+    private ListView<Route> routeListView;
+
+    private final ObservableList<Route> routes =
+            FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+
+        routeListView.setItems(routes);
+    }
 }

2566b64a7ffda5c50b13b6ec492687ba3d96b635
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-08-15 03:19:20 +0800

    Initialized Route REST API with create and list endpoints

--- src/main/java/ph/edu/dlsu/anilink/backend/RouteController.java
+++ src/main/java/ph/edu/dlsu/anilink/backend/RouteController.java
@@ -1,4 +1,35 @@
 package ph.edu.dlsu.anilink.backend;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ph.edu.dlsu.anilink.model.Route;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/routes")
+
 public class RouteController {
+    private final List<Route> routes = new ArrayList<>();
+
+    // GET all routes
+    @GetMapping
+    public List<Route> getAllRoutes() {
+        return routes;
+    }
+
+    // CREATE a new route
+    @PostMapping
+    public ResponseEntity<Route> createRoute(
+            @RequestBody Route route) {
+
+        routes.add(route);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(route);
+    }
 }

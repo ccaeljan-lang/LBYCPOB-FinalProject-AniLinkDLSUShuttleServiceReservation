0793af9a7740b50ed753a7c63d20920736f717ff
caeljandcristobal <ccaeljan@gmail.com>
2026-08-15 23:44:10 +0800

    changed route to generate id

--- src/main/java/ph/edu/dlsu/anilink/model/Route.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Route.java
@@ -1,12 +1,21 @@
 package ph.edu.dlsu.anilink.model;
 
 public class Route {
+
     private Long routeId;
     private String origin;
     private String destination;
 
+    public Route() {
+    }
+
     public Route(Long routeId, String origin, String destination) {
         this.routeId = routeId;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public Route(String origin, String destination) {
         this.origin = origin;
         this.destination = destination;
     }
@@ -15,15 +24,32 @@ public class Route {
         return routeId;
     }
 
+    public void setRouteId(Long routeId) {
+        this.routeId = routeId;
+    }
+
     public String getOrigin() {
         return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
     }
 
     public String getDestination() {
         return destination;
     }
 
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
     public String getRouteName() {
         return origin + " -> " + destination;
+    }
+
+    @Override
+    public String toString() {
+        return getRouteName();
     }
 }

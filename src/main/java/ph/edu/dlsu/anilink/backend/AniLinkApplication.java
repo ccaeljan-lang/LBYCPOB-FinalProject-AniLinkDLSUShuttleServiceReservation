8c23affaf99b44f59214d30078b6244b4ffcd3f2
caeljandcristobal <ccaeljan@gmail.com>
2026-08-15 22:15:10 +0800

    test run on AniLinkApplication.java

--- src/main/java/ph/edu/dlsu/anilink/backend/AniLinkApplication.java
+++ src/main/java/ph/edu/dlsu/anilink/backend/AniLinkApplication.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.anilink.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AniLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AniLinkApplication.class, args);
+    }
 }

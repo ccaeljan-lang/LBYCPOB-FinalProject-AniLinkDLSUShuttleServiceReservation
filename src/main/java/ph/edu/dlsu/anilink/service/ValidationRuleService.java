64b7a4e40b5149a647b184fda3c2faaf235c3d9c
caeljandcristobal <ccaeljan@gmail.com>
2026-08-16 00:23:45 +0800

    feat: implement ValidationRuleService

--- src/main/java/ph/edu/dlsu/anilink/service/ValidationRuleService.java
+++ src/main/java/ph/edu/dlsu/anilink/service/ValidationRuleService.java
@@ -1,4 +1,46 @@
 package ph.edu.dlsu.anilink.service;
 
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.anilink.interfaces.BookingRule;
+import ph.edu.dlsu.anilink.model.Trip;
+import ph.edu.dlsu.anilink.model.User;
+import ph.edu.dlsu.anilink.rules.CategoryLimitRule;
+import ph.edu.dlsu.anilink.rules.RouteLimitRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ValidationRuleService {
+
+    private final List<BookingRule> rules = new ArrayList<>();
+
+    public ValidationRuleService() {
+        rules.add(new CategoryLimitRule());
+        rules.add(new RouteLimitRule());
+    }
+
+    public boolean validateBooking(User user, Trip trip) {
+
+        for (BookingRule rule : rules) {
+
+            if (!rule.validate(user, trip)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String getValidationError(User user, Trip trip) {
+
+        for (BookingRule rule : rules) {
+
+            if (!rule.validate(user, trip)) {
+                return rule.getErrorMessage();
+            }
+        }
+
+        return null;
+    }
 }

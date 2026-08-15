a3a920dae6809b7fcff8cb7acbb1fb292066325a
de-rekk <painganshawnderek@gmail.com>
2026-08-15 22:23:20 +0800

    Implemented SupabaseService constructor to initialize RestClient

--- src/main/java/ph/edu/dlsu/anilink/service/SupabaseService.java
+++ src/main/java/ph/edu/dlsu/anilink/service/SupabaseService.java
@@ -5,3 +5,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+@Service
+public class SupabaseService {
+    private final RestClient restClient;
+
+    public SupabaseService(
+            @Value("${app.supabase.url}") String supabaseUrl,
+            @Value("${app.supabase.secret-key}") String secretKey) {
+
+        this.restClient = RestClient.builder()
+                .baseUrl(supabaseUrl + "/rest/v1")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("apikey", secretKey)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + secretKey)
+                .build();
+    }
+}

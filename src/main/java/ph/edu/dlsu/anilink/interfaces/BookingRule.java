9273fb44021efdade89fe1d1f4c7cccb9b3ef7da
caeljandcristobal <ccaeljan@gmail.com>
2026-08-15 22:06:40 +0800

    changed booking rule class to an interface

--- src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
+++ src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
@@ -3,7 +3,7 @@ package ph.edu.dlsu.anilink.interfaces;
 import ph.edu.dlsu.anilink.model.Trip;
 import ph.edu.dlsu.anilink.model.User;
 
-public class BookingRule {
+public interface BookingRule {
     boolean validate(User user, Trip trip);
     String getErrorMessage();
 }

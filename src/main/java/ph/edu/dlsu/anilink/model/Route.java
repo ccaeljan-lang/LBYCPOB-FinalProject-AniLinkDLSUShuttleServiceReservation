3f8945234c3d9d679d04a5670b673cddb484dac5
de-rekk <painganshawnderek@gmail.com>
2026-08-16 17:34:36 +0800

    Implemented Route model

--- src/main/java/ph/edu/dlsu/anilink/model/Route.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Route.java
@@ -1,55 +1,49 @@
 package ph.edu.dlsu.anilink.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+
 public class Route {
 
+    @JsonProperty("id")
     private Long routeId;
+
+    @JsonProperty("origin")
     private String origin;
+
+    @JsonProperty("destination")
     private String destination;
 
-    public Route() {
+    public Route() {}
+
+    public Long getRouteId() { return routeId; }
+    public void setRouteId(Long routeId) { this.routeId = routeId; }
+
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
+
+    // EQUALS & HASHCODE ARE REQUIRED FOR JAVAFX COMBOBOX MATCHING
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Route route = (Route) o;
+        return Objects.equals(routeId, route.routeId);
     }
 
-    public Route(Long routeId, String origin, String destination) {
-        this.routeId = routeId;
-        this.origin = origin;
-        this.destination = destination;
-    }
-
-    public Route(String origin, String destination) {
-        this.origin = origin;
-        this.destination = destination;
-    }
-
-    public Long getRouteId() {
-        return routeId;
-    }
-
-    public void setRouteId(Long routeId) {
-        this.routeId = routeId;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getRouteName() {
-        return origin + " -> " + destination;
+    @Override
+    public int hashCode() {
+        return Objects.hash(routeId);
     }
 
     @Override
     public String toString() {
-        return getRouteName();
+        if (origin != null && destination != null) {
+            return origin + " ↔ " + destination;
+        }
+        return "Unknown Route";
     }
 }

8c5ae9f14cec635b274646b5ff004839b0f43a95
caeljandcristobal <ccaeljan@gmail.com>
2026-08-18 22:44:30 +0800

    added javadoc comments to Driver.java

--- src/main/java/ph/edu/dlsu/anilink/model/Driver.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Driver.java
@@ -3,6 +3,19 @@ package ph.edu.dlsu.anilink.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Domain model representing a Driver user within the AniLink system.
+ *
+ * <p>This class extends {@link User} to manage credentials and operational attributes for shuttle drivers.
+ * It encapsulates the following components:
+ * <ul>
+ *   <li><b>License Number:</b> Stores the official driving license string assigned to the driver.</li>
+ *   <li><b>Role Resolution:</b> Overrides user role resolution to enforce fixed {@code DRIVER} permissions across the system.</li>
+ *   <li><b>JSON Mapping:</b> Uses Jackson annotations ({@link JsonCreator}, {@link JsonProperty}) to map properties like
+ *       {@code license_number} directly during API requests with Supabase.</li>
+ * </ul>
+ * </p>
+ */
 public class Driver extends User {
 
     private String licenseNumber;

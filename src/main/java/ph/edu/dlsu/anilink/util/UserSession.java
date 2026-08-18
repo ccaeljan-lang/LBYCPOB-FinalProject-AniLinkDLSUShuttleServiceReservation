37281a7725d52aa25d9aa2e01b89aefae9e63318
caeljandcristobal <ccaeljan@gmail.com>
2026-08-18 23:01:14 +0800

    added javadoc comments for UserSession.java

--- src/main/java/ph/edu/dlsu/anilink/util/UserSession.java
+++ src/main/java/ph/edu/dlsu/anilink/util/UserSession.java
@@ -3,6 +3,16 @@ package ph.edu.dlsu.anilink.util;
 import org.springframework.stereotype.Component;
 import ph.edu.dlsu.anilink.model.User;
 
+/**
+ * In-memory session manager for tracking the active application user.
+ *
+ * <p>Key components:
+ * <ul>
+ *   <li><b>State Management:</b> Holds a reference to the logged-in {@link User} instance across FX controllers.</li>
+ *   <li><b>Lifecycle Control:</b> Provides methods to update and purge session state during login and logout workflows.</li>
+ * </ul>
+ * </p>
+ */
 @Component
 public class UserSession {
     private User currentUser;

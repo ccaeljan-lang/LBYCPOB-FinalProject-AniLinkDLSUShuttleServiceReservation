f20f9be4da72552e74f3c0f8083af3802b0fca71
caeljandcristobal <ccaeljan@gmail.com>
2026-08-18 22:40:21 +0800

    added javadoc comments to Trackable.java

--- src/main/java/ph/edu/dlsu/anilink/interfaces/Trackable.java
+++ src/main/java/ph/edu/dlsu/anilink/interfaces/Trackable.java
@@ -1,6 +1,22 @@
 package ph.edu.dlsu.anilink.interfaces;
 
+/**
+ * Interface defining contract behavior for domain entities capable of real-time tracking.
+ */
 public interface Trackable {
+
+    /**
+     * Updates the geographic location coordinates of the trackable entity.
+     *
+     * @param lat latitude coordinate
+     * @param lng longitude coordinate
+     */
     void updateLocation(double lat, double lng);
+
+    /**
+     * Updates the current operational status of the trackable entity.
+     *
+     * @param status string representation of the new status
+     */
     void updateStatus(String status);
 }

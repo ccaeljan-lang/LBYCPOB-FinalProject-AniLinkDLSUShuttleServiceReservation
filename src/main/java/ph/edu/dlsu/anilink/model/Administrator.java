16106adcaa05604b5701dd54839809720702cdb2
caeljandcristobal <ccaeljan@gmail.com>
2026-08-18 22:42:27 +0800

    added javadoc comments to Administrator.java

--- src/main/java/ph/edu/dlsu/anilink/model/Administrator.java
+++ src/main/java/ph/edu/dlsu/anilink/model/Administrator.java
@@ -3,6 +3,23 @@ package ph.edu.dlsu.anilink.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Domain model representing an Administrator user within the AniLink system.
+ *
+ * <p>This class extends {@link User} to specialize functionality for system administrators.
+ * It encapsulates administrative metadata including:
+ * <ul>
+ *   <li><b>Department:</b> Tracks the specific administrative unit or department assigned to the admin.</li>
+ *   <li><b>Role Management:</b> Overrides user role resolution to enforce fixed {@code ADMIN} privileges.</li>
+ *   <li><b>JSON Serialization:</b> Integrates Jackson annotations ({@link JsonCreator}, {@link JsonProperty})
+ *       to support direct JSON mapping during REST interactions with Supabase.</li>
+ * </ul>
+ * </p>
+ *
+ * @author AniLink Development Team
+ * @version 1.0
+ * @see ph.edu.dlsu.anilink.model.User
+ */
 public class Administrator extends User {
 
     private String department;

448fd1472f0d99715f1d4b58db1e62d1cdf6060c
caeljandcristobal <ccaeljan@gmail.com>
2026-08-18 22:37:45 +0800

    added javadoc comments

--- src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
+++ src/main/java/ph/edu/dlsu/anilink/interfaces/BookingRule.java
@@ -3,7 +3,24 @@ package ph.edu.dlsu.anilink.interfaces;
 import ph.edu.dlsu.anilink.model.Trip;
 import ph.edu.dlsu.anilink.model.User;
 
+/**
+ * Strategy interface defining validation rules for processing trip reservations.
+ */
 public interface BookingRule {
+
+    /**
+     * Validates whether a user is eligible to book a specific trip.
+     *
+     * @param user the user attempting to make a reservation
+     * @param trip the target trip for the reservation
+     * @return true if the booking satisfies the rule, false otherwise
+     */
     boolean validate(User user, Trip trip);
+
+    /**
+     * Retrieves the error message explaining why the validation failed.
+     *
+     * @return a descriptive error message string
+     */
     String getErrorMessage();
 }
